b7066ee49bc437858264d305bc91323e7b2ee456
Rob <rschlub@gmail.com>
2018-03-01 21:29:48 -0800

    removed old non-dev scripts

--- run_node.sh
+++
@@ -1,4 +0,0 @@
-cd node
-docker build -t test-node .
-docker run -it --rm -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/app:/app/shared --name dev-node test-node
-# docker run -it --rm test-node
