8c567b779d284af4be9988aba97f64fc8e9b82c7
Rob <rschlub@gmail.com>
2018-03-03 07:43:31 -0800

    Dockerfiles all in root

--- run_container.sh
+++ run_container.sh
@@ -1,14 +1,32 @@
 DOCKERFILE='Dockerfile_prod'
+PORT=5000
 if [ $1 ]
 then
   DOCKERFILE=Dockerfile_$1
 fi
 
-# echo $DOCKERFILE
+if [ $1 = "prod" ]
+then
+  PORT=5001
+fi
+
+if [ $1 = "python" ]
+then
+  PORT=5002
+fi
+
+if [ $1 = "js" ]
+then
+  PORT=5003
+fi
+# echo $DOCKERFILE $PORT
 cp $DOCKERFILE Dockerfile
 
 docker build -t test-$1 .
 
-
-docker run -it --rm -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/shared:/app/shared --name dev-$1 test-$1
+docker run -it --rm \
+    -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/shared:/app/shared \
+    --name dev-$1 \
+    -p $PORT:5000 \
+    test-$1
 
--- run_dev_python.sh
+++
@@ -1,7 +0,0 @@
-cd python
-docker build -t test-python .
-cd ..
-
-docker run -it --rm -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/shared:/app/shared --name dev-python test-python
-
-
