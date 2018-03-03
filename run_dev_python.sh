b1018fed4200cafdf694d6031371fdaa2b8ff575
Rob <rschlub@gmail.com>
2018-03-03 07:04:23 -0800

    Project Reorg to include tests separate to app

--- run_dev_python.sh
+++ run_dev_python.sh
@@ -2,6 +2,6 @@ cd python
 docker build -t test-python .
 cd ..
 
-docker run -it --rm -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/app:/app/shared --name dev-python test-python
+docker run -it --rm -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/shared:/app/shared --name dev-python test-python
 
 
