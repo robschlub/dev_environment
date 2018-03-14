aa0906470d8eddabb13de517c23225a639c451e9
Rob <rschlub@gmail.com>
2018-03-14 09:21:45 -0700

    debug jenkins uglify 23

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
-
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // eslint-disable-line import/no-unresolved
+const UglifyJsPlugin = require('/app/node_modules/uglifyjs-webpack-plugin'); // eslint-disable-line import/no-unresolved
 
 const config = {
   entry: './shared/app/app/static/src/test.js',
@@ -23,33 +22,33 @@ const config = {
       },
     ],
   },
-  // plugins: [
-  //   new UglifyJsPlugin({
-  //     uglifyOptions: {
-  //       ecma: 8,
-  //       warnings: false,
-  //       // parse: { ...options },
-  //       // compress: { ...options },
-  //       // mangle: {
-  //       //   ...options,
-  //       //   properties: {
-  //       //     // mangle property options
-  //       //   },
-  //       // },
-  //       output: {
-  //         comments: false,
-  //         beautify: false,
-  //         // ...options
-  //       },
-  //       toplevel: false,
-  //       nameCache: null,
-  //       ie8: false,
-  //       keep_classnames: undefined,
-  //       keep_fnames: false,
-  //       safari10: false,
-  //     },
-  //   }),
-  // ],
+  plugins: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        ecma: 8,
+        warnings: false,
+        // parse: { ...options },
+        // compress: { ...options },
+        // mangle: {
+        //   ...options,
+        //   properties: {
+        //     // mangle property options
+        //   },
+        // },
+        output: {
+          comments: false,
+          beautify: false,
+          // ...options
+        },
+        toplevel: false,
+        nameCache: null,
+        ie8: false,
+        keep_classnames: undefined,
+        keep_fnames: false,
+        safari10: false,
+      },
+    }),
+  ],
   mode: 'development', // Make this production to remove eval statements from bundle output
 };
 
