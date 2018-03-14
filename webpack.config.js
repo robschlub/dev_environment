f7a27628308b5515e8eba5cd1791b2e746494d89
Rob <rschlub@gmail.com>
2018-03-14 07:45:29 -0700

    debug jenkins uglify 10

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const config = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         use: 'babel-loader',
       },
     ],
