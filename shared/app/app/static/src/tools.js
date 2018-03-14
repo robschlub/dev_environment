1434daf5a8ec46f60dce6569f6331f090f6190f4
Rob <rschlub@gmail.com>
2018-03-14 16:51:57 -0700

    Add eslint support for jest

--- shared/app/app/static/src/tools.js
+++ shared/app/app/static/src/tools.js
@@ -1,14 +1,20 @@
 // @flow
 
-export const Console = (text: string) => {
+const Console = (text: string) => {
   console.log(text); // eslint-disable-line no-console
 };
 
-export function add(a: number, b: number): number {
+function add(a: number, b: number): number {
   return a + b;
 }
 
-export function mulToString(a: number, b: number): string {
+function mulToString(a: number, b: number): string {
   return (a * b).toString();
 }
+
+const divide = (a: number, b: number): number => a / b;
+
+export { divide, mulToString, add, Console };
+// export dividedBy;
+// export dividedBy;
 // export { Console as default };
