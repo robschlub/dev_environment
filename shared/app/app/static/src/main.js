34da6875c395d20163ba6a6a38d90dea2229cacc
Rob <rschlub@gmail.com>
2018-03-15 11:16:43 -0700

    eslint fixes

--- shared/app/app/static/src/main.js
+++ shared/app/app/static/src/main.js
@@ -5,8 +5,8 @@ import * as tools from './tools';
 tools.Console(tools.add(2, 10).toString());
 tools.Console(tools.mulToString(2, 5).toString());
 
-const myId:HTMLElement | null = document.getElementById("intro");
+const myId:HTMLElement | null = document.getElementById('intro');
 
-if (myId instanceof HTMLElement){
+if (myId instanceof HTMLElement) {
   myId.innerHTML = '<p>Hello from JS</p>';
 }
