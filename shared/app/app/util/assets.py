c2321b79aff9d3a0b9097685c7a566baaf865f77
Rob <rschlub@gmail.com>
2018-03-15 16:28:44 -0700

    loading tools in dev - but console not writing

--- shared/app/app/util/assets.py
+++ shared/app/app/util/assets.py
@@ -3,13 +3,21 @@ from .. import app
 
 bundles = {
 
-    'compiled_map_js': Bundle(
-        'build/main.bundle.js.map',
-        output='dist/main.flask.bundle.js.map'),
+    # 'compiled_map_js': Bundle(
+    #     'build/main.bundle.js.map',
+    #     output='dist/main.bundle.js.map'),
 
-    'compiled_js': Bundle(
+    'main_js': Bundle(
         'build/main.bundle.js',
-        output='dist/main.flask.bundle.js'),
+        output='dist/main.bundle.js'),
+
+    # 'tools_map_js': Bundle(
+    #     'build/tools.bundle.js.map',
+    #     output='dist/tools.bundle.js.map'),
+
+    'tools_js': Bundle(
+        'build/tools.bundle.js',
+        output='dist/tools.bundle.js'),
 }
 
 assets = Environment(app)
