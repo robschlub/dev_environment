f206ec999da9be4746047112957f4c51b6589e96
Rob <rschlub@gmail.com>
2018-03-15 17:54:58 -0700

    More simple docker container for heroku

--- run_container.sh
+++ run_container.sh
@@ -1,28 +1,33 @@
 DOCKERFILE='Dockerfile_prod'
 PORT=5000
-if [ $1 ]
+if [ $1 ];
 then
   DOCKERFILE=Dockerfile_$1
 fi
 
-if [ $1 = "prod" ]
+if [ $1 = "prod" ];
 then
   PORT=5000
 fi
 
-if [ $1 = "flask" ]
+if [ $1 = "flask" ];
 then
   PORT=5001
 fi
 
-if [ $1 = "python" ]
+if [ $1 = "python" ];
 then
   PORT=5002
 fi
 
-if [ $1 = "js" ]
+if [ $1 = "js" ];
 then
   PORT=5003
+fi
+
+if [ $1 = "heroku" ];
+then
+  PORT=5001
 fi
 
 cp $DOCKERFILE Dockerfile
@@ -31,13 +36,20 @@ docker build -t test-$1 .
 
 rm Dockerfile
 
-docker run -it --rm \
+if [ $1 = 'heroku' ];
+then
+  
+  docker run -it --rm \
+    --name dev-$1 \
+    -p $PORT:5000 \
+    test-$1
+else
+  docker run -it --rm \
     -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/shared:/app/shared \
     -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/webpack.config.js:/app/webpack.config.js \
     -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/.eslintrc.json:/app/.eslintrc.json \
     --name dev-$1 \
     -p $PORT:5000 \
     test-$1
-
-
+fi
 
