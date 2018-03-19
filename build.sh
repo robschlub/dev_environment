f83750781b577be17ba7c660b61595069328cc9e
Rob <rschlub@gmail.com>
2018-03-19 11:16:11 -0700

    add build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+MODE=prod
+
+if [ $1 = "dev" ];
+then
+  MODE=dev
+fi
+
+if [ $1 = "stage" ];
+then
+  MODE=stage
+fi
+
+cp setup/Dockerfile_dev Dockerfile
+
+docker build -t build-temp .
+
+rm Dockerfile
+
+docker run -it --rm \
+    -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/shared:/app/shared \
+    --name build-temp \
+    --entrypoint "npm" \
+    build-temp \
+    -c run webpack -- --env.mode=$MODE
+
+
