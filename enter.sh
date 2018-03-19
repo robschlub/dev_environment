62b79c24ba7900f29debe3e37363f1d0d6e7dc4f
Rob <rschlub@gmail.com>
2018-03-19 10:26:14 -0700

    Move dockerfiles, webpack, requirements into setup. Reduce environments to just dev, stage, prod

--- enter.sh
+++ enter.sh
@@ -8,47 +8,29 @@ fi
 if [ $1 = "prod" ];
 then
   HOST_PORT=5000
-  CONTAINTER_PORT=5000
+  CONTAINTER_PORT=4000
 fi
 
-if [ $1 = "flask" ];
+if [ $1 = "stage" ];
 then
   HOST_PORT=5001
   CONTAINTER_PORT=5000
 fi
 
-if [ $1 = "python" ];
+if [ $1 = "dev" ];
 then
   HOST_PORT=5002
   CONTAINTER_PORT=5000
 fi
 
-if [ $1 = "js" ];
-then
-  HOST_PORT=5003
-  CONTAINTER_PORT=5000
-fi
-
-if [ $1 = "heroku" ];
-then
-  HOST_PORT=5001
-  CONTAINTER_PORT=4000
-fi
-
-if [ $1 = "nodepy" ];
-then
-  HOST_PORT=5005
-  CONTAINTER_PORT=5000
-fi
-
-cp $DOCKERFILE Dockerfile
+cp setup/$DOCKERFILE Dockerfile
 
 GUNICORN_PORT=4000
 docker build -t test-$1 .
 
 rm Dockerfile
 
-if [ $1 = 'heroku' ];
+if [ $1 = 'prod' ];
 then
   docker run -it --rm \
     --name dev-$1 \
@@ -58,8 +40,7 @@ then
 else
   docker run -it --rm \
     -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/shared:/app/shared \
-    -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/webpack.config.js:/app/webpack.config.js \
-    -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/.eslintrc.json:/app/.eslintrc.json \
+    -v /Users/rob/Dropbox/Programming/Repositories/Test_Projects/dev_environment/setup/webpack.config.js:/app/webpack.config.js \
     --name dev-$1 \
     -p $HOST_PORT:$CONTAINTER_PORT \
     test-$1
