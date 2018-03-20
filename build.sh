f7e9385bc48a65e8e5096c2b54c0abcd4c5aaa20
Rob <rschlub@gmail.com>
2018-03-20 15:03:43 -0700

    move env variables into build.sh - make release-candidate travis

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ MODE=prod
 HOST_PATH=`pwd`
 HEROKU_APP_NAME=itgeti              # Production app name on Heroku
 HEROKU_DEV_APP_NAME=itgetidev       # Dev app name on Heroku
-DEPLOY_PROD_BRANCH=travis           # Branch to test and deploy to prod
-DEPLOY_DEV_BRANCH=release-candidate # Branch to test and deploy to dev
+DEPLOY_PROD_BRANCH=master           # Branch to test and deploy to prod
+DEPLOY_DEV_BRANCH=travis # Branch to test and deploy to dev
 
 # Setup colors and text formatting
 red=`tput setaf 1`
