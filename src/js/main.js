b9f10a83539f4224a6e4fe40a31f9ac105e55599
Rob <rschlub@gmail.com>
2018-03-22 16:47:03 -0700

    add simple jumbotron

--- src/js/main.js
+++ src/js/main.js
@@ -11,31 +11,43 @@ const myId:HTMLElement | null = document.getElementById('intro');
 
 if (myId instanceof HTMLElement) {
   ReactDOM.render(
-    <div className="container-fluid">
-      {component}
-      <div>Helloooooo</div>
-      <div className="row">
-        <div className="col test">
-          hello1
+    <div className="jumbotron jumbotron-fluid text-xs-center"
+    style={{
+             backgroundImage: 'url(static/flower.jpg',
+             backgroundSize: 'cover',
+             backgroundPosition: 'top',
+           }}>
+      <div className="container">
+
+        <h1>Helloooooo</h1>
+
+        {component}
+
+        <div className="row">
+          <div className="col test">
+            hello1
+          </div>
+          <div className="col-6">
+            hello2</div>
+          <div className="col">
+            hello3
+          </div>
         </div>
-        <div className="col-6">
-          hello2</div>
-        <div className="col">
-          hello3
+        <div className="row">
+          <div className="btn btn-primary">
+            <p>Hello world, from react using css!</p>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="btn btn-primary">
-          <p>Hello world, from react using css!</p>
+        <div className="row">
+          <div className="btn btn-primary redbox">
+            <p>Test</p>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="btn btn-primary redbox">
-          <p>Test</p>
-        </div>
-      </div>
+        <div className="row">{component}</div>
+        <div className="row">{component}</div>
+        <div className="row">{component}</div>
+      </div>,
     </div>,
-    // </div>,
     myId,
   );
 }
