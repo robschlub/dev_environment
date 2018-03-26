f75f3c80c913b3a878ee5ffba07782ae44d626e4
Rob <rschlub@gmail.com>
2018-03-26 11:08:42 -0700

    functioning bootstrap css

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,47 @@ const myId:HTMLElement | null = document.getElementById('intro');
 if (myId instanceof HTMLElement) {
   ReactDOM.render(
     <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Home</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Select Topic
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">Shapes</a>
+                <a className="dropdown-item" href="#">Circles</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Glossary</a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+        <div className="dropdown">
+          <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+          <span className="caret"></span></button>
+          <ul className="dropdown-menu">
+            <li><a href="#">HTML</a></li>
+            <li><a href="#">CSS</a></li>
+            <li><a href="#">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
+
       <Jumbotron className="jumbotron -fluid splash"
                   containerFluid={false}
                   >
