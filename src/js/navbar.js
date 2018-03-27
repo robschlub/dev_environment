949a30301d49d758588d9e971822ef0a2e59c0e7
Rob <rschlub@gmail.com>
2018-03-26 20:47:29 -0700

    move search bar into collapsable content

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -26,7 +26,7 @@ export default class Navbar extends React.Component
         <a className="navbar-brand" href="/">
           <img src="/static/icon-lg.png" width="30" height="30" className="d-inline-block align-top homeicon" alt=""/>
         </a>
-
+        
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -58,12 +58,13 @@ export default class Navbar extends React.Component
               </div>
             </li>
           </ul>
-        </div>
+        
 
-        <form className="form-inline">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+          <form className="form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
       </nav>;
 
     return <div {...props}>
