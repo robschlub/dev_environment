fe7d9d5b6d064d1cabcafe787aef6c5d42af0b9a
Rob <rschlub@gmail.com>
2018-03-26 17:42:49 -0700

    separate home page

--- app/app/routes.py
+++ app/app/routes.py
@@ -14,9 +14,13 @@ from app import app
 
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+
 @app.route('/introduction')
-def index():
-    return render_template('index.html')
+def introduction():
+    return render_template('introduction.html')
 
 
 @app.route('/about')
