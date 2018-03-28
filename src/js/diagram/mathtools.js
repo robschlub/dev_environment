1ade923ee24992344497b39248e71e4662dc95de
Rob <rschlub@gmail.com>
2018-03-28 10:04:37 -0700

    Add 2D matrix library, tests, and mathtools library and initial tests

---
+++ src/js/diagram/mathtools.js
@@ -0,0 +1,29 @@
+// @flow
+
+const roundValue = (value: number, precision: number = 5) => {
+  const multiplier = 10 ** precision;
+  // if (value.constructor == d2.point(0,0).constructor) {}
+  let result = Math.round(value * multiplier) / multiplier;
+  if (Object.is(result, -0)) {
+    result = 0;
+  }
+  return result;
+};
+
+const round = (arrayOrValue: number | Array<number>, precision: number = 5) => {
+  let result = 0;
+  if (arrayOrValue instanceof Array) {
+    result = [];
+    // result = arrayOrValue.map(elem, round(elem, precision));
+    for (let i = 0, j = arrayOrValue.length; i < j; i += 1) {
+      result[i] = round(arrayOrValue[i], precision);
+    }
+  // } else if (arrayOrValue.constructor == d2.point(0,0).constructor){
+  //     result = roundPoint(arrayOrValue, precision);
+  } else {
+    result = roundValue(arrayOrValue, precision);
+  }
+  return result;
+};
+
+export default round;
---
+++ src/js/diagram/mathtools.test.js
@@ -0,0 +1,25 @@
+import round from './mathtools';
+
+describe('Math tools testing', () => {
+  // Rounding a value
+  test('Round a value that doesn\'t need rounding', () => {
+    expect(round(10.0)).toEqual(10.0);
+  });
+
+  test('Round a value up to nearest whole number', () => {
+    expect(round(9.51, 0)).toEqual(10.0);
+  });
+
+  test('Round a value at half way mark up to nearest whole number', () => {
+    expect(round(9.5, 0)).toEqual(10.0);
+  });
+
+  test('Round a value down to nearest whole number', () => {
+    expect(round(10.49, 0)).toEqual(10.0);
+  });
+
+  // Round an array
+  test('Round an array of numbers to 1st decimal place', () => {
+    expect(round([0.5, 0.9, 1.2, 2.3], 0)).toEqual([1, 1, 1, 2]);
+  });
+});
