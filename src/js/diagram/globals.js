930bd38c21676162f68462281e08f58694849c5f
Rob <rschlub@gmail.com>
2018-03-30 10:45:00 -0700

    Complete globals tests

--- src/js/diagram/globals.js
+++ src/js/diagram/globals.js
@@ -1,188 +1,73 @@
 // @flow
 
+import getColors from './colors';
+
 class GlobalVariables {
-  data: Array<number>;
+  animationId: AnimationFrameID;
+  drawScene: () => mixed;
+  canvas: HTMLElement | null;
+  isTouchDevice: boolean;
+  colors: {};
+  animateNextFrameFlag: boolean;
+  requestNextAnimationFrame: (()=>mixed) => AnimationFrameID;
+  colors: Object;
+  canvasRect: ClientRect | null;
+  canvasWidth: number;
+  canvasHeight: number;
   static instance: Object;
 
   constructor() {
     if (!GlobalVariables.instance) {
-      this.data = [];
+      // this.animationId = 0;
+      this.drawScene = () => {};
+      this.updateCanvas(null);
+      this.isTouchDevice = (
+        'ontouchstart' in window            // works on most browsers
+        || navigator.maxTouchPoints > 1     // works on IE10/11 and Surface
+      );
+      this.requestNextAnimationFrame = (
+        window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame
+      );
+      this.colors = getColors();
+      this.animateNextFrameFlag = false;
       GlobalVariables.instance = this;
     }
 
     return GlobalVariables.instance;
   }
 
-  add(item: number) {
-    this.data.push(item);
+  setDrawMethod(drawMethod: () => mixed) {
+    this.drawScene = drawMethod;
   }
 
-  get(id: number) {
-    return this.data.find(d => d.id === id);
+  animateNextFrame() {
+    cancelAnimationFrame(this.animationId);
+    const nextFrame = this.requestNextAnimationFrame(this.drawScene);
+    this.animationId = nextFrame;
   }
-}
 
-const instance = new GlobalVariables();
-Object.freeze(instance);
-
-export default instance;
-
-
-
-
-function GlobalVariablesObject() {
-  this.animationId = 0;
-  this.drawScene = null;
-  this.canvas = null;
-  this.isTouchDevice = (
-    'ontouchstart' in window          // works on most browsers
-    || navigator.maxTouchPoints       // works on IE10/11 and Surface
-  );
-
-  
-  let _this = this;
-
-  const requestNextAnimationFrame = window.requestAnimationFrame || 
-            window.mozRequestAnimationFrame || 
-            window.webkitRequestAnimationFrame || 
-            window.msRequestAnimationFrame;
-
-  // const elem = document.getElementsByTagName("body")[0];
-  // const style = window.getComputedStyle(elem);
-  this.dimensions = {};
-
-  let cssProperties = [
-  '--dim-diagram-height',
-  '--dim-diagram-width',
-  '--dim-learning-text-width',
-  '--dim-canvas-width',
-  '--dim-canvas-height',
-  '--dim-header-height',
-  '--dim-title-height',
-  '--dim-subtitle-height',
-  '--dim-navbar_height',
-
-  '--color-stick',
-  '--color-arc',
-  '--color-rotation',
-  '--color-hint_arrow',
-  '--color-corners',
-  '--color-shapes',
-  '--color-axes',
-  '--color-portions',
-  '--color-anchor',
-  '--color-anchor_text',
-  '--color-corners_more_sharp',
-  '--color-corners_less_sharp',
-  '--color-angle',
-  '--color-angle_text',
-  '--color-start_stick',
-  ]
-
-  this.colors = {};
-  this.animateNextFrameFlag = false;
-
-  // this.colors['stick'] = style.getPropertyValue('--color-stick');
-  // this.colors['corners'] = style.getPropertyValue('--color-corners');
-  // console.log(style.getPropertyValue('--color-stick'))
-  // console.log(style)
-  for (let i=0,j=cssProperties.length;i<j;++i) {
-    let styleElem = cssProperties[i];
-    if(styleElem.substr(0,8) == '--color-'){
-      let colorName = styleElem.substr(8);
-      let colString = style.getPropertyValue(styleElem)
-      // console.log(colString)
-      // if(colorName == 'corners_more_sharp'){
-      //   document.getElementById('headerbar').innerHTML = colString;
-      //   colString = colString.replace(/.*\(/i,'')
-      //   colString = colString.replace(/\)/i,'')
-      //   document.getElementById('headerbar').innerHTML = colString;
-      // }
-      // colString = colString.substr(5,colString.length-6).split(",");
-      colString = colString.replace(/.*\(/i,'')
-      colString = colString.replace(/\)/i,'')
-      colString = colString.split(",");
-      // console.log(colString)
-      // if(colorName == 'corners_more_sharp'){
-        // document.getElementById('headerbar').innerHTML = colString;
-      // }
-      // console.log(colorName)
-      for (let i=0, j=3;i<j;++i) {
-        // console.log(colString[i]);
-        colString[i] /= 255;
-
-      }
-      if (colString.length == 3) {
-        colString.push(1);
-      }
-      this.colors[colorName]= colString;
-      // console.log(colorName)
-    }
-
-    if(styleElem.substr(0,6) == '--dim-'){
-      let dimName = styleElem.substr(6);
-      let dimString = style.getPropertyValue(styleElem)
-      this.dimensions[dimName] = dimString.substr(0,dimString.length-2);    
-      // console.log(dimString)
+  updateCanvas(canvas: HTMLElement | null) {
+    this.canvas = canvas;
+    if (canvas instanceof HTMLElement) {
+      this.canvasRect = canvas.getBoundingClientRect();
+      this.canvasWidth = canvas.scrollWidth;
+      this.canvasHeight = canvas.scrollHeight;
+    } else {
+      this.canvasRect = null;
+      this.canvasWidth = 0;
+      this.canvasHeight = 0;
     }
   }
 
-  // for (let color in this.colors){
-  //   this.colors[color] = this.colors[color].substr(5,this.colors[color].length-6).split(",");
-  //   for (let i=0, j=this.colors[color].length;i<j;++i) {
-  //     this.colors[color][i] /= 255;
-  //   }
+  // getColors() {
+  //   this.colors = getColors();
   // }
-
-
-  function setDrawMethod(drawMethod) {
-    _this.drawScene = drawMethod;
-  }
-
-  function animateNextFrame() {
-    // console.log("Asdf");
-    cancelAnimationFrame(_this.animationId);
-    _this.animationId = requestNextAnimationFrame(_this.drawScene);
-  }
-
-  function updateCanvas(canvas) {
-    _this.canvas = canvas;
-    // _this.canvas = document.getElementById("c");
-    _this.canvasRect = _this.canvas.getBoundingClientRect();
-    _this.canvasWidth = _this.canvas.scrollWidth;
-    _this.canvasHeight = _this.canvas.scrollHeight;
-
-  }
-
-  return {
-  animationId:  this.animationId,
-  animateNextFrame:   animateNextFrame,
-  setDrawMethod:   setDrawMethod, 
-    dimensions:   this.dimensions,
-    colors:     this.colors,
-    updateCanvas:  updateCanvas,
-    canvas:     this.canvas,
-  }
-
 }
 
+// const globalvars: Object = new GlobalVariables();
+// Object.freeze(globalvars);
 
-// Global Variables Singleton
-var GlobalVariables = (function () {
-  "use strict";
-  var instance;
-
-  function createInstance() {
-    var object = new GlobalVariablesObject();
-    return object;
-  }
- 
-  return {
-    getInstance: function () {
-      if (!instance) {
-        instance = createInstance();
-      }
-      return instance;
-    },
-  }
-})();
+export default GlobalVariables;
