f4e0b043b2ba6e4717d48f1f9e80663c878ff9a4
Rob <rschlub@gmail.com>
2018-04-03 11:58:09 -0700

    add animation helpers to Mathtools

--- src/js/diagram/mathtools.js
+++ src/js/diagram/mathtools.js
@@ -21,7 +21,60 @@ const round = (arrayOrValue: number | Array<number>, precision: number = 5) => {
   return result;
 };
 
+const decelerate = (velocity: number, deceleration: number, time: number) => {
+  const sign = velocity / Math.abs(velocity);
+  const newVelocity = velocity - sign * deceleration * time;
+  const newSign = newVelocity / Math.abs(newVelocity);
+  if (newSign !== sign) {
+    return 0;
+  }
+  return newVelocity;
+};
+
+const easeinout = (percentTime: number) => {
+  const x = percentTime;
+  const percentDistance = (x ** 2) / ((x ** 2) + ((1 - x) ** 2));
+  return percentDistance;
+};
+
+function easeout(percentTime: number) {
+  const x = 0.5 + percentTime / 2;
+  const power = 2;
+  const percentDistance = (x ** power) / ((x ** power) + ((1 - x) ** power));
+  return (percentDistance - 0.5) * 2;
+}
+function easein(percentTime: number) {
+  const x = percentTime / 2;
+  const power = 2;
+  const percentDistance = (x ** power) / ((x ** power) + ((1 - x) ** power));
+  return percentDistance * 2;
+}
+
+function sinusoid(
+  deltaTime: number = 1,
+  frequency: number = 1,
+  bias: number = 0,
+  mag: number = 1,
+  phaseOffset: number = 0,
+) {
+  return bias + mag * Math.sin(deltaTime * frequency * 2.0 * Math.PI + phaseOffset);
+}
+// const animationPhase = (transform, time, rotDirection = 0, animationStyle = easeinout) => {
+//     return {
+//         transform: transform.copy(),
+//         time: time,
+//         rotDirection: rotDirection,
+//         animationStyle: animationStyle,
+//     }
+// }
+
 export {
   round,
   roundNum,
+  decelerate,
+  easeinout,
+  easeout,
+  easein,
+  sinusoid,
 };
+
--- src/js/diagram/mathtools.test.js
+++ src/js/diagram/mathtools.test.js
@@ -1,4 +1,4 @@
-import { round } from './mathtools';
+import { round, decelerate, easeinout } from './mathtools';
 
 describe('Math tools testing', () => {
   // Rounding a value
@@ -25,5 +25,53 @@ describe('Math tools testing', () => {
 
   test('Round 1 element array', () => {
     expect(round([0.2])).toEqual([0.2]);
+  });
+
+  describe('decelerate', () => {
+    test('dec 1m/s/s for 1s', () => {
+      const v1 = 10;   // m/s
+      const time = 1;         // s
+      const dec = 1;          // m/s/s
+      const v2 = decelerate(v1, dec, time);
+      expect(v2).toBe(9);
+    });
+    test('dec 1m/s/s for 2s', () => {
+      const v1 = 10;   // m/s
+      const time = 2;         // s
+      const dec = 1;          // m/s/s
+      const v2 = decelerate(v1, dec, time);
+      expect(v2).toBe(8);
+    });
+    test('dec 1m/s/s for 8s', () => {
+      const v1 = 10;   // m/s
+      const time = 8;         // s
+      const dec = 1;          // m/s/s
+      const v2 = decelerate(v1, dec, time);
+      expect(v2).toBe(2);
+    });
+    test('dec 1m/s/s for 11s', () => {
+      const v1 = 10;   // m/s
+      const time = 11;         // s
+      const dec = 1;          // m/s/s
+      const v2 = decelerate(v1, dec, time);
+      expect(v2).toBe(0);
+    });
+  });
+  describe('easeinout', () => {
+    test('0', () => {
+      expect(easeinout(0)).toBe(0);
+    });
+    test('1', () => {
+      expect(easeinout(1)).toBe(1);
+    });
+    test('0.5', () => {
+      expect(easeinout(0.5)).toBe(0.5);
+    });
+    test('0.25', () => {
+      expect(easeinout(0.25)).toBe(0.1);
+    });
+    test('0.75', () => {
+      expect(easeinout(0.75)).toBe(0.9);
+    });
   });
 });
