b590879d46cf81fff6e990a4ffc374fa632a9983
Rob <rschlub@gmail.com>
2018-04-15 19:15:09 -0700

    Custom arrow test added

--- src/js/diagram/vertexObjects/Arrow.test.js
+++ src/js/diagram/vertexObjects/Arrow.test.js
@@ -6,7 +6,6 @@ import { round } from '../mathtools';
 describe('Arrow', () => {
   test('Default', () => {
     const arrow = new Arrow(webgl);
-
     const border = [
       new Point(0, 0), new Point(-0.5, -0.5),
       new Point(-0.25, -0.5), new Point(-0.25, -1),
@@ -22,26 +21,32 @@ describe('Arrow', () => {
       0.25, -0.5,
       0.5, -0.5,
     ];
-
     expect(round(arrow.points)).toEqual(points);
     expect(arrow.border[0].map(x => x.round())).toEqual(border);
   });
-  // test('Tip at 1, -1', () => {
-  //   const tri = new IsoTriangle(webgl, 1, 1.5, new Point(1, -1));
-
-  //   const border = [
-  //     new Point(1, -1),
-  //     new Point(0.5, -2.5),
-  //     new Point(1.5, -2.5),
-  //     new Point(1, -1),
-  //   ];
-  //   const points = [
-  //     1, -1,
-  //     0.5, -2.5,
-  //     1.5, -2.5,
-  //   ];
-
-  //   expect(round(tri.points)).toEqual(points);
-  //   expect(tri.border[0].map(x => x.round())).toEqual(border);
-  // });
+  test('Custom', () => {
+    const arrow = new Arrow(webgl, 2, 1, 4, 2, new Point(1, -1));
+    const border = [
+      new Point(1, -1),
+      new Point(0, -3),
+      new Point(0.5, -3),
+      new Point(0.5, -5),
+      new Point(1.5, -5),
+      new Point(1.5, -3),
+      new Point(2, -3),
+      new Point(1, -1),
+    ];
+    const points = [
+      1, -1,
+      0, -3,
+      0.5, -3,
+      0.5, -5,
+      1.5, -5,
+      1.5, -3,
+      2, -3,
+    ];
+    expect(round(arrow.points)).toEqual(points);
+    expect(arrow.border[0].map(x => x.round())).toEqual(border);
+  });
 });
+
