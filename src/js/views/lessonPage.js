5609ff48d3d8d5ac98a1b64fc856ad2b7ed1acbd
Rob <rschlub@gmail.com>
2018-04-25 12:12:49 -0700

    page cleanup

--- src/js/views/lessonPage.js
+++ src/js/views/lessonPage.js
@@ -17,17 +17,28 @@ const lessonPage = () => {
     ReactDOM.render(
       <div>
         <Navbar active='Lesson'/>
-        <Content />
-        {/*<div className='container'>*/}
           <div className='main_page'>
-            <Canvas id="my_Canvas" didMountFn={testgl} />
-            <div className='button_container'>
-              <Button label="Prev" className="-primary -lesson" />
-              <Button label="Go To" className="-primary -lesson" />
-              <Button label="Next" className="-primary -lesson" />
+            <div className='lesson_container'>
+              <div className='lesson_title'>
+                Shapes and Corners
+              </div>
+              <p className='lesson_text'>
+                Many shapes have corners.
+              </p>
+              <p className='lesson_text'>
+                The sharpness of the corner is a property that can describe a
+                shape. This is a long sentence
+              </p>
+              <Canvas id="my_Canvas" didMountFn={testgl} />
+              {/* 
+              <div className='button_container'>
+                <Button label="Prev" className="-primary -lesson" />
+                <Button label="Go To" className="-primary -lesson" />
+                <Button label="Next" className="-primary -lesson" />
+              </div>
+              */}
             </div>
           </div>
-        {/*</div>*/}
       </div>,
       lessonId,
     );
