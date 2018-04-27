da3b3d171c9c79eeec5ea5ef17d3f942c8ed85ec
Rob <rschlub@gmail.com>
2018-04-27 11:42:04 -0700

    radius easier to touch

--- src/js/Lesson/circleDiagram.js
+++ src/js/Lesson/circleDiagram.js
@@ -18,11 +18,20 @@ type typeCircleDiagramCollection = {
   _anchor: DiagramElementPrimative;
 } & DiagramElementCollection ;
 
-// function makeAnchor(shapes: Object, location: Point) {
-//   const anchor = shapes.polygon(vertices, true, lineWidth, lineColor);
+function makeRadius(shapes: Object, location: Point) {
+  const radius = shapes.horizontalLine(
+    new Point(0, 0), circleRadius, lineWidth,
+    0, radiusColor, location,
+  );
+  radius.isTouchable = true;
+  radius.isMovable = true;
 
-//   return square;
-// }
+  for (let i = 0; i < radius.vertices.border[0].length; i += 1) {
+    radius.vertices.border[0][i].y *= 10;
+  }
+  return radius;
+}
+
 // $FlowFixMe
 class CircleDiagram extends Diagram {
   elements: typeCircleDiagramCollection;
@@ -30,15 +39,13 @@ class CircleDiagram extends Diagram {
   constructor(id: string) {
     super(`${id}_container`, -1, -1, 2, 2);
   }
+
   createDiagramElements() {
     const { shapes } = this;
     this.elements = shapes.collection();
     const origin = new Point(0, 0);
 
-    const radius = shapes.horizontalLine(
-      origin, circleRadius, lineWidth,
-      0, radiusColor, origin,
-    );
+    const radius = makeRadius(shapes, origin);
     this.add('radius', radius);
 
     const anchor = shapes.polygonFilled(
@@ -46,6 +53,9 @@ class CircleDiagram extends Diagram {
       anchorPoints, anchorColor, origin,
     );
     this.add('anchor', anchor);
+
+    this.elements.isTouchable = true;
+    this.elements.isMovable = true;
   }
 }
 
