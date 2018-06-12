1dc26de2aa2e6b61cd76d7bf46d786461a42ef1b
Rob <rschlub@gmail.com>
2018-06-12 13:24:54 -0700

    fix another bug in update_paths

--- containers/dev/update_paths.py
+++ containers/dev/update_paths.py
@@ -1,5 +1,6 @@
 import os
 import re
+
 lesson_path = './src/Lessons/'
 file_extensions = ['js', 'scss']
 path_matches = ['js', 'css']
@@ -13,14 +14,14 @@ def replace_paths(fileName, depth):
     for line in lines:
         new_line = line
         for path_match in path_matches:
-            m = re.search(f'^ *import.*from \'.*\.\.\/{path_match}', new_line)
+            m = re.search(f'^@* *import.*\'.*\.\.\/{path_match}', new_line)
+            if not m:
+                m = re.search(f'^ *from *\'.*\.\.\/{path_match}', new_line)
             if m:
-                path = re.search(r'\'.*\/js\/', new_line)
+                path = re.search(f'\'.*\/{path_match}\/', new_line)
                 if path:
                     # print(depth_string)
-                    new_line = re.sub(r'\'.*\/js\/', f'\'{depth_string}{path_match}/', new_line)
-                    # print(line)
-                    # print(new_line)
+                    new_line = re.sub(f'\'.*\/{path_match}\/', f'\'{depth_string}{path_match}/', new_line)
         new_lines.append(new_line)
     f.close()
     f = open(fileName, 'w')
@@ -33,9 +34,5 @@ for root, dirs, files in os.walk(lesson_path):
         name, ext = os.path.splitext(file)
         ext = ext.strip('.')
         depth = len(root.split(os.path.sep)) - 2
-        # print(f'{root.split(os.path.sep)} {depth} {file} {ext}')
         if ext in file_extensions:
             replace_paths(os.path.join(root, file), depth)
-    # print(root, "consumes", end=" ")
-    # print(sum(getsize(join(root, name)) for name in files), end=" ")
-    # print("bytes in", len(files), "non-directory files")
