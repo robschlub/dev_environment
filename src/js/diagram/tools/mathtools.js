79176ed3cfe5b25d6538b238c7cbecb20e902869
Robert Schlub <>
2018-06-21 15:19:24 -0700

    fix flow errors

--- src/js/diagram/tools/mathtools.js
+++ src/js/diagram/tools/mathtools.js
@@ -12,6 +12,7 @@ const roundNum = (value: number, precision: number = 5) => {
 const round = (arrayOrValue: number | Array<number>, precision: number = 5) => {
   let result = 0;
   if (arrayOrValue instanceof Array) {
+    // $FlowFixMe
     result = arrayOrValue.map(elem => round(elem, precision));
   // } else if (arrayOrValue.constructor == d2.point(0,0).constructor){
   //     result = roundPoint(arrayOrValue, precision);
