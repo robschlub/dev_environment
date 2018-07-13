e807aac4a5970a57d0ba74e8d174d1b5456f2634
Robert Schlub <>
2018-07-13 10:21:36 -0700

    changes transferred into important-angles

--- src/Lessons/Math/DegreesAndRadians/layout.js
+++ src/Lessons/Math/DegreesAndRadians/layout.js
@@ -50,7 +50,7 @@ export default function lessonLayout() {
   };
   layout.arcEquation = {
     left: new Point(-2.3, -1.2),
-    leftMiddle: new Point(-2.5, -1.1),
+    // leftMiddle: new Point(-2.5, -1.1),
     centerTop: new Point(-1.0, 0.9),
     centerBottom: new Point(-0.5, -1.0),
     center: new Point(-0.5, 0),
--- src/Lessons/Math/ImportantAngles/diagram.js
+++ src/Lessons/Math/ImportantAngles/diagram.js
@@ -1,110 +1,21 @@
 // @flow
-import Diagram from '../../../js/diagram/Diagram';
 import { DiagramElementCollection } from '../../../js/diagram/Element';
 import CircleCollection from './diagramCollectionCircle';
 import type { circleCollectionType } from './diagramCollectionCircle';
-import { Point, minAngleDiff, Transform } from '../../../js/diagram/tools/g2';
 import lessonLayout from './layout';
+import AngleCircleDiagram from '../../../LessonsCommon/AngleCircle/diagram';
 
-const layout = lessonLayout();
-const { colors } = layout;
-const backgroundColor = colors.diagram.background;
-
-
-// type circleCollectionType = {
-//   _anchor: DiagramElementPrimative;
-//   _arc: DiagramElementPrimative;
-//   _angle: DiagramElementPrimative;
-//   _radius: DiagramElementPrimative;
-//   _reference: DiagramElementPrimative;
-//   _radialLinesA: DiagramElementPrimative;
-//   _radialLinesB: DiagramElementCollection;
-//   _radialLinesDeg: DiagramElementCollection;
-//   // resize: () => void;
-// } & DiagramElementCollection;
 
 type typeElements = {
   _circle: circleCollectionType;
-  // +resize: () => void;
 } & DiagramElementCollection ;
 
 // $FlowFixMe
-class LessonDiagram extends Diagram {
+class LessonDiagram extends AngleCircleDiagram {
   elements: typeElements;
 
   constructor(id: string) {
-    const { limits } = lessonLayout();
-    super(
-      `${id}`,
-      limits.left,
-      limits.bottom,
-      limits.width,
-      limits.height,
-      backgroundColor,
-    );
-  }
-  createDiagramElements() {
-    const circleCollection = new CircleCollection(
-      this,
-      new Transform().translate(0, 0),
-    );
-    this.elements = circleCollection;
-
-    this.elements.isTouchable = true;
-    this.elements.isMovable = true;
-    this.fontScale = 1.2;
-  }
-
-  resize() {
-    const { limits } = layout;
-    this.limits = limits;
-    this.elements.updateLimits(limits);
-    super.resize();
-  }
-
-  touchMoveHandler(
-    previousClientPoint: Point,
-    currentClientPoint: Point,
-  ): boolean {
-    if (this.beingMovedElements.length === 0) {
-      return false;
-    }
-    // if (!this.elements._circle.show) {
-    //   return super.touchMoveHandler(previousClientPoint, currentClientPoint);
-    // }
-    if (this.elements._circle._radius.state.isBeingMoved) {
-      let center = this.elements._circle.transform.t();
-      if (center === null || center === undefined) {
-        center = new Point(0, 0);
-      }
-      const previousPixelPoint = this.clientToPixel(previousClientPoint);
-      const currentPixelPoint = this.clientToPixel(currentClientPoint);
-
-      const previousDiagramPoint =
-        previousPixelPoint.transformBy(this.pixelToDiagramSpaceTransform.matrix());
-      const currentDiagramPoint =
-        currentPixelPoint.transformBy(this.pixelToDiagramSpaceTransform.matrix());
-      const currentAngle = Math.atan2(
-        currentDiagramPoint.y - center.y,
-        currentDiagramPoint.x - center.x,
-      );
-      const previousAngle = Math.atan2(
-        previousDiagramPoint.y - center.y,
-        previousDiagramPoint.x - center.x,
-      );
-      const diffAngle = minAngleDiff(previousAngle, currentAngle);
-      const transform = this.elements._circle._radius.transform.copy();
-      const rot = transform.r();
-      if (rot != null) {
-        transform.updateRotation(rot - diffAngle);
-        this.elements._circle._radius.moved(transform);
-      }
-    } else {
-      return super.touchMoveHandler(previousClientPoint, currentClientPoint);
-    }
-
-    this.animateNextFrame();
-    return true;
+    super(id, lessonLayout(), CircleCollection);
   }
 }
 
