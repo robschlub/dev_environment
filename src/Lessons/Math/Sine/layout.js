1425fe56b1f8ab3d46a371ac03ac1bc79545c3ee
Robert Schlub <>
2018-07-17 14:08:47 -0700

    colors consistently strings

--- src/Lessons/Math/Sine/layout.js
+++ src/Lessons/Math/Sine/layout.js
@@ -10,6 +10,7 @@ const cssColorNames = [
 /* eslint-disable key-spacing, comma-spacing, no-multi-spaces, space-in-parens */
 export default function lessonLayout() {
   const colors = getCssColors(cssColorNames);
+  console.log(colors)
   const layout: Object = sinCosCircleLayout();
   layout.colors = Object.assign(colors, layout.colors);
   return layout;
