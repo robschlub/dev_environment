be41989fd9a545154749305405f15683b3d97ce7
Robert Schlub <>
2018-07-17 10:45:24 -0700

    copy of important angles into new sine lesson

---
+++ src/Lessons/Math/Sine/lesson.js
@@ -0,0 +1,8 @@
+// // @flow
+
+import '../../../css/style.scss';
+import renderLesson from '../../../js/views/lesson/lesson';
+import Content from './content';
+import './style.scss';
+
+renderLesson(new Content('shapes'));
