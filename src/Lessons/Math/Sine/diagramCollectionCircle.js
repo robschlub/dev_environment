c3afddc05ba17328b7a502ebdcfbc79d3026b868
Robert Schlub <>
2018-07-17 11:18:12 -0700

    basic quadrants working in diagram

--- src/Lessons/Math/Sine/diagramCollectionCircle.js
+++ src/Lessons/Math/Sine/diagramCollectionCircle.js
@@ -14,34 +14,35 @@ type rightAngleType = {
 
 export type extendedCircleType = {
   _rightAngle: rightAngleType;
-  _acuteRange: DiagramElementPrimative;
-  _obtuseRange: DiagramElementPrimative;
-  _reflexRange: DiagramElementPrimative;
+  _quad1: DiagramElementPrimative;
+  _quad2: DiagramElementPrimative;
+  _quad3: DiagramElementPrimative;
+  _quad4: DiagramElementPrimative;
   _axes: {
     _x: DiagramElementPrimative;
     _y: DiagramElementPrimative;
   } & DiagramElementCollection;
 } & circleType;
 
-type angleTypes = 'acute' | 'obtuse' | 'right' | 'reflex' | 'straight';
+type quadrantType = 1 | 2 | 3 | 4;
 
 type varStateExtendedType = {
-    angleSelected: angleTypes;
+    quadrant: quadrantType;
   } & varStateType;
 
-export type ImportantAnglesCollectionType = {
+export type SineCollectionType = {
   _circle: extendedCircleType;
   varState: varStateExtendedType;
   enableAutoChange: boolean;
-  angleTypes: Array<string>;
+  angleTypes: Array<number>;
 };
 
-class ImportantAnglesCollection extends AngleCircle {
+class SineCollection extends AngleCircle {
   _circle: extendedCircleType;
   // numSections: Array<number>;
   varState: varStateExtendedType;
   enableAutoChange: boolean;
-  angleTypes: Array<string>;
+  angleTypes: Array<number>;
 
   makeRightAngle() {
     const rad = this.layout.angleRadius * 0.9;
@@ -83,44 +84,29 @@ class ImportantAnglesCollection extends AngleCircle {
     return axes;
   }
 
-  makeAcuteRange() {
+  makeQuad(num: number = 1) {
     return this.shapes.polygonFilled(
       this.layout.anglePoints, this.layout.axes.length, 0,
       this.layout.anglePoints / 4, this.colors.angleArea, new Transform()
-        .translate(0, 0),
-    );
-  }
-
-  makeObtuseRange() {
-    return this.shapes.polygonFilled(
-      this.layout.anglePoints, this.layout.axes.length, 0,
-      this.layout.anglePoints / 4, this.colors.angleArea, new Transform()
-        .rotate(Math.PI / 2)
-        .translate(0, 0),
-    );
-  }
-
-  makeReflexRange() {
-    return this.shapes.polygonFilled(
-      this.layout.anglePoints, this.layout.axes.length, 0,
-      this.layout.anglePoints / 2, this.colors.angleArea, new Transform()
-        .rotate(Math.PI)
-        .translate(0, 0),
+        .translate(0, 0)
+        .rotate((num - 1) * Math.PI / 2),
     );
   }
 
   addToCircle() {
     this._circle.add('rightAngle', this.makeRightAngle());
-    this._circle.add('acuteRange', this.makeAcuteRange());
-    this._circle.add('obtuseRange', this.makeObtuseRange());
-    this._circle.add('reflexRange', this.makeReflexRange());
     this._circle.add('axes', this.makeAxes());
+    this._circle.add('quad1', this.makeQuad(1));
+    this._circle.add('quad2', this.makeQuad(2));
+    this._circle.add('quad3', this.makeQuad(3));
+    this._circle.add('quad4', this.makeQuad(4));
     this._circle.order = [
-      ...this._circle.order.slice(-4, -1),
-      ...this._circle.order.slice(0, 2),
-      ...this._circle.order.slice(-1),
-      ...this._circle.order.slice(2, -4),
+      ...this._circle.order.slice(-4),
+      // ...this._circle.order.slice(0, 2),
+      // ...this._circle.order.slice(-1),
+      ...this._circle.order.slice(0, -4),
     ];
+    console.log(this._circle.order)
   }
 
   constructor(diagram: Diagram, transform: Transform = new Transform()) {
@@ -129,109 +115,98 @@ class ImportantAnglesCollection extends AngleCircle {
     this.varState = {
       radialLines: 4,
       rotation: 0,
-      angleSelected: 'acute',
+      quadrant: 1,
     };
     this.enableAutoChange = true;
     this.addToCircle();
-    this.angleTypes = ['acute', 'obtuse', 'right', 'reflex', 'straight'];
+    this.angleTypes = [1, 2, 3, 4];
   }
 
   // eslint-disable-next-line class-methods-use-this
-  calcAngleType(angle: number, thresholds: Object): angleTypes {
-    if (angle >= thresholds.acute.min && angle <= thresholds.acute.max) {
-      return 'acute';
+  calcQuadrant(angle: number, thresholds: Object): quadrantType {
+    if (angle >= thresholds.quad1.min && angle <= thresholds.quad1.max) {
+      return 1;
     }
-    if (angle >= thresholds.right.min && angle <= thresholds.right.max) {
-      return 'right';
+    if (angle >= thresholds.quad2.min && angle <= thresholds.quad2.max) {
+      return 2;
     }
-    if (angle >= thresholds.obtuse.min && angle <= thresholds.obtuse.max) {
-      return 'obtuse';
+    if (angle >= thresholds.quad3.min && angle <= thresholds.quad3.max) {
+      return 3;
     }
-    if (angle >= thresholds.straight.min && angle <= thresholds.straight.max) {
-      return 'straight';
-    }
-    return 'reflex';
+    return 4;
   }
 
-  calcAngleTypeDegrees(angle: number): angleTypes {
+  calcQuadrantDegrees(angle: number): quadrantType {
     const thresholds = {
-      acute: {
+      quad1: {
         min: 0,
         max: 89,
       },
-      right: {
+      quad2: {
         min: 90,
-        max: 90,
-      },
-      obtuse: {
-        min: 91,
         max: 179,
       },
-      straight: {
+      quad3: {
         min: 180,
-        max: 180,
+        max: 269,
       },
-      reflex: {
-        min: 181,
-        max: 360,
+      quad4: {
+        min: 270,
+        max: 359,
       },
     };
-    return this.calcAngleType(angle, thresholds);
+    return this.calcQuadrant(angle, thresholds);
   }
 
-  calcAngleTypeRadians(angle: number): angleTypes {
+  calcQuadrantRadians(angle: number): quadrantType {
     const thresholds = {
-      acute: {
+      quad1: {
         min: 0,
         max: 1.56,
       },
-      right: {
+      quad2: {
         min: 1.57,
-        max: 1.57,
-      },
-      obtuse: {
-        min: 1.58,
         max: 3.13,
       },
-      straight: {
+      quad3: {
         min: 3.14,
-        max: 3.14,
+        max: 4.70,
       },
-      reflex: {
-        min: 3.15,
-        max: 6.28,
+      quad4: {
+        min: 4.71,
+        max: 6.27,
       },
     };
-    return this.calcAngleType(angle, thresholds);
+    return this.calcQuadrant(angle, thresholds);
   }
 
-  toggleUnits(toUnit: 'rad' | 'deg' | null) {
-    const elemDeg = document.getElementById('id_degrees');
-    const elemRad = document.getElementById('id_radians');
-    let unit = toUnit;
-    if (toUnit === null) {
-      if (this.varState.radialLines === 360) {
-        unit = 'rad';
-      } else {
-        unit = 'deg';
-      }
-    }
-    if (elemDeg != null && elemRad != null) {
-      if (unit === 'rad') {
-        this.hideDegrees();
-        this.showRadians();
-        elemDeg.classList.remove('lesson__important_angles_unit_selected');
-        elemRad.classList.add('lesson__important_angles_unit_selected');
-        this.setParagraphUnits('rad');
-      } else if (unit === 'deg') {
-        this.hideRadians();
-        this.showDegrees();
-        elemRad.classList.remove('lesson__important_angles_unit_selected');
-        elemDeg.classList.add('lesson__important_angles_unit_selected');
-        this.setParagraphUnits('deg');
-      }
-    }
-  }
+  // toggleUnits(toUnit: 'rad' | 'deg' | null) {
+  //   const elemDeg = document.getElementById('id_degrees');
+  //   const elemRad = document.getElementById('id_radians');
+  //   let unit = toUnit;
+  //   if (toUnit === null) {
+  //     if (this.varState.radialLines === 360) {
+  //       unit = 'rad';
+  //     } else {
+  //       unit = 'deg';
+  //     }
+  //   }
+  //   if (elemDeg != null && elemRad != null) {
+  //     if (unit === 'rad') {
+  //       this.hideDegrees();
+  //       this.showRadians();
+  //       elemDeg.classList.remove('lesson__important_angles_unit_selected');
+  //       elemRad.classList.add('lesson__important_angles_unit_selected');
+  //       this.setParagraphUnits('rad');
+  //     } else if (unit === 'deg') {
+  //       this.hideRadians();
+  //       this.showDegrees();
+  //       elemRad.classList.remove('lesson__important_angles_unit_selected');
+  //       elemDeg.classList.add('lesson__important_angles_unit_selected');
+  //       this.setParagraphUnits('deg');
+  //     }
+  //   }
+  // }
 
 
   setParagraphUnits(onUnit: 'rad' | 'deg') {
@@ -262,99 +237,86 @@ class ImportantAnglesCollection extends AngleCircle {
     const r = this.varState.rotation;
     if (this.enableAutoChange) {
       if (this.varState.radialLines === 360) {
-        const angleType = this.calcAngleTypeDegrees(Math.round(r * 180 / Math.PI));
-        // this.selectAngle(angleType);
-        // this.showText(angleType);
-        this.showAngleType(angleType);
+        const quad = this.calcQuadrantDegrees(Math.round(r * 180 / Math.PI));
+        this.showQuadrant(quad);
       } else if (this.varState.radialLines === Math.PI * 2) {
-        const angleType = this.calcAngleTypeRadians(Math.round(r * 100) / 100);
-        // this.selectAngle(angleType);
-        // this.showText(angleType);
-        this.showAngleType(angleType);
+        const quad = this.calcQuadrantRadians(Math.round(r * 100) / 100);
+        this.showQuadrant(quad);
       }
     }
   }
 
-  selectAngle(angleType: angleTypes) {
-    let elem;
-    if (angleType !== this.varState.angleSelected) {
-      elem = document.getElementById(`id_${this.varState.angleSelected}`);
-      if (elem != null) {
-        elem.classList.remove('lesson__important_angles_table_selected');
-      }
-    }
-    elem = document.getElementById(`id_${angleType}`);
-    if (elem != null) {
-      elem.classList.add('lesson__important_angles_table_selected');
-    }
-    if (this.varState.angleSelected === 'right' && angleType !== 'right') {
-      this.toggleRightAngleLine(false);
-    }
-    this.varState.angleSelected = angleType;
+  selectAngle(quadrant: quadrantType) {
+    // let elem;
+    // if (quadrant !== this.varState.quadrant) {
+    //   elem = document.getElementById(`id_${this.varState.quadrant}`);
+    //   if (elem != null) {
+    //     elem.classList.remove('lesson__important_angles_table_selected');
+    //   }
+    // }
+    // elem = document.getElementById(`id_${quadrant}`);
+    // if (elem != null) {
+    //   elem.classList.add('lesson__important_angles_table_selected');
+    // }
+    // if (this.varState.quadrant === 'right' && quadrant !== 'right') {
+    //   this.toggleRightAngleLine(false);
+    // }
+    this.varState.quadrant = quadrant;
   }
 
-  showAngleType(angleType: angleTypes) {
-    this.selectAngle(angleType);
-    this.showText(angleType);
-    if (angleType !== 'acute') {
-      this._circle._acuteRange.hide();
-    }
-    if (angleType !== 'obtuse') {
-      this._circle._obtuseRange.hide();
-    }
-    if (angleType !== 'reflex') {
-      this._circle._reflexRange.hide();
-    }
-    if (angleType === 'acute') {
-      this._circle._acuteRange.show();
-    }
-    if (angleType === 'obtuse') {
-      this._circle._obtuseRange.show();
-    }
-    if (angleType === 'reflex') {
-      this._circle._reflexRange.show();
-    }
-  }
-
-  goToAcute() {
-    const angle45 = Math.random() * Math.PI / 4 * 0.95;
-    let angle = angle45;
-    const r = this._circle._radius.transform.r();
-    if (r != null) {
-      if (this.varState.angleSelected !== 'acute') {
-        angle = Math.PI / 4;
-      } else if (r < Math.PI / 4) {
-        angle += Math.PI / 4;
-      }
-      this.rotateToAngleDisablingAutoChange(angle);
-    }
-    // this.selectAngle('acute');
-    // this.showText('acute');
-    // this._circle._acuteRange.show();
-    this.showAngleType('acute');
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  showText(angleType: angleTypes) {
-    const ids = [
-      'id_acute_text',
-      'id_obtuse_text',
-      'id_right_text',
-      'id_straight_text',
-      'id_reflex_text',
-    ];
-    ids.forEach((id) => {
-      if (id !== angleType) {
-        const elem = document.getElementById(id);
-        if (elem != null) {
-          elem.classList.add('lesson__important_angles_text_hide');
-        }
+  showQuadrant(quadrant: quadrantType) {
+    this.selectAngle(quadrant);
+    this.showText(quadrant);
+    const circle = this._circle;
+    const quads = [circle._quad1, circle._quad2, circle._quad3, circle._quad4];
+    quads.forEach((q, index) => {
+      if (quadrant !== index + 1) {
+        q.hide();
+      } else {
+        q.show();
       }
     });
-    const elem = document.getElementById(`id_${angleType}_text`);
-    if (elem != null) {
-      elem.classList.remove('lesson__important_angles_text_hide');
-    }
+  }
+
+  // goToAcute() {
+  //   const angle45 = Math.random() * Math.PI / 4 * 0.95;
+  //   let angle = angle45;
+  //   const r = this._circle._radius.transform.r();
+  //   if (r != null) {
+  //     if (this.varState.angleSelected !== 'acute') {
+  //       angle = Math.PI / 4;
+  //     } else if (r < Math.PI / 4) {
+  //       angle += Math.PI / 4;
+  //     }
+  //     this.rotateToAngleDisablingAutoChange(angle);
+  //   }
+  //   // this.selectAngle('acute');
+  //   // this.showText('acute');
+  //   // this._circle._acuteRange.show();
+  //   this.showAngleType('acute');
+  // }
+
+  // eslint-disable-next-line class-methods-use-this
+  showText(quadrant: quadrantType) {
+    // const ids = [
+    //   'id_acute_text',
+    //   'id_obtuse_text',
+    //   'id_right_text',
+    //   'id_straight_text',
+    //   'id_reflex_text',
+    // ];
+    // ids.forEach((id) => {
+    //   if (id !== angleType) {
+    //     const elem = document.getElementById(id);
+    //     if (elem != null) {
+    //       elem.classList.add('lesson__important_angles_text_hide');
+    //     }
+    //   }
+    // });
+    // const elem = document.getElementById(`id_${angleType}_text`);
+    // if (elem != null) {
+    //   elem.classList.remove('lesson__important_angles_text_hide');
+    // }
   }
 
   rotateToAngleDisablingAutoChange(angle: number) {
@@ -366,87 +328,87 @@ class ImportantAnglesCollection extends AngleCircle {
     }));
   }
 
-  goToRight() {
-    this.enableAutoChange = false;
-    const angle = Math.PI / 2;
-    this.rotateToAngleDisablingAutoChange(angle);
-    // this.selectAngle('right');
-    // this.showText('right');
-    this.showAngleType('right');
-  }
+  // goToRight() {
+  //   this.enableAutoChange = false;
+  //   const angle = Math.PI / 2;
+  //   this.rotateToAngleDisablingAutoChange(angle);
+  //   // this.selectAngle('right');
+  //   // this.showText('right');
+  //   this.showAngleType('right');
+  // }
 
-  goToObtuse() {
-    const angle45 = Math.random() * Math.PI / 4 * 0.95;
-    let angle = angle45;
-    const r = this._circle._radius.transform.r();
-    if (r != null) {
-      if (this.varState.angleSelected !== 'obtuse') {
-        angle = 3 * Math.PI / 4;
-      } else if (r < 3 * Math.PI / 4) {
-        angle += 3 * Math.PI / 4;
-      } else {
-        angle += Math.PI / 2;
-      }
-      this.rotateToAngleDisablingAutoChange(angle);
-    }
-    // this.selectAngle('obtuse');
-    // this.showText('obtuse');
-    this.showAngleType('obtuse');
-  }
-  goToStraight() {
-    const angle = Math.PI;
-    this.rotateToAngleDisablingAutoChange(angle);
-    // this.selectAngle('straight');
-    // this.showText('straight');
-    this.showAngleType('straight');
-  }
-  goToReflex() {
-    const angle90 = Math.random() * Math.PI / 2 * 0.95;
-    let angle = angle90;
-    const r = this._circle._radius.transform.r();
-    if (r != null) {
-      if (r < 3 * Math.PI / 2) {
-        angle += 3 * Math.PI / 2;
-      } else {
-        angle += Math.PI;
-      }
-      this.rotateToAngleDisablingAutoChange(angle);
-    }
-    // this.selectAngle('reflex');
-    // this.showText('reflex');
-    this.showAngleType('reflex');
-  }
+  // goToObtuse() {
+  //   const angle45 = Math.random() * Math.PI / 4 * 0.95;
+  //   let angle = angle45;
+  //   const r = this._circle._radius.transform.r();
+  //   if (r != null) {
+  //     if (this.varState.angleSelected !== 'obtuse') {
+  //       angle = 3 * Math.PI / 4;
+  //     } else if (r < 3 * Math.PI / 4) {
+  //       angle += 3 * Math.PI / 4;
+  //     } else {
+  //       angle += Math.PI / 2;
+  //     }
+  //     this.rotateToAngleDisablingAutoChange(angle);
+  //   }
+  //   // this.selectAngle('obtuse');
+  //   // this.showText('obtuse');
+  //   this.showAngleType('obtuse');
+  // }
+  // goToStraight() {
+  //   const angle = Math.PI;
+  //   this.rotateToAngleDisablingAutoChange(angle);
+  //   // this.selectAngle('straight');
+  //   // this.showText('straight');
+  //   this.showAngleType('straight');
+  // }
+  // goToReflex() {
+  //   const angle90 = Math.random() * Math.PI / 2 * 0.95;
+  //   let angle = angle90;
+  //   const r = this._circle._radius.transform.r();
+  //   if (r != null) {
+  //     if (r < 3 * Math.PI / 2) {
+  //       angle += 3 * Math.PI / 2;
+  //     } else {
+  //       angle += Math.PI;
+  //     }
+  //     this.rotateToAngleDisablingAutoChange(angle);
+  //   }
+  //   // this.selectAngle('reflex');
+  //   // this.showText('reflex');
+  //   this.showAngleType('reflex');
+  // }
 
-  pulseAngle() {
-    if (this._circle._rightAngle.isShown) {
-      this._circle._rightAngle._horizontal.pulseScaleNow(1, 2);
-      this._circle._rightAngle._vertical.pulseScaleNow(1, 2);
-    } else {
-      this._circle._angle._arc.pulseThickNow(1, 1.04, 7);
-      this._circle._angle._arrow.pulseScaleNow(1, 1.5);
-    }
-    this.diagram.animateNextFrame();
-  }
-  toggleRightAngleLine(show: boolean | null) {
-    let toShow = show;
-    if (show === null) {
-      if (this._circle._rightAngle.isShown) {
-        toShow = false;
-      } else {
-        toShow = true;
-      }
-    }
-    if (toShow) {
-      this._circle._rightAngle.showAll();
-      this._circle._angle.hideAll();
-      this._circle._rightAngle._horizontal.pulseScaleNow(1, 2);
-      this._circle._rightAngle._vertical.pulseScaleNow(1, 2);
-    } else {
-      this._circle._rightAngle.hideAll();
-      this._circle._angle.showAll();
-    }
-    this.diagram.animateNextFrame();
-  }
+  // pulseAngle() {
+  //   if (this._circle._rightAngle.isShown) {
+  //     this._circle._rightAngle._horizontal.pulseScaleNow(1, 2);
+  //     this._circle._rightAngle._vertical.pulseScaleNow(1, 2);
+  //   } else {
+  //     this._circle._angle._arc.pulseThickNow(1, 1.04, 7);
+  //     this._circle._angle._arrow.pulseScaleNow(1, 1.5);
+  //   }
+  //   this.diagram.animateNextFrame();
+  // }
+  // toggleRightAngleLine(show: boolean | null) {
+  //   let toShow = show;
+  //   if (show === null) {
+  //     if (this._circle._rightAngle.isShown) {
+  //       toShow = false;
+  //     } else {
+  //       toShow = true;
+  //     }
+  //   }
+  //   if (toShow) {
+  //     this._circle._rightAngle.showAll();
+  //     this._circle._angle.hideAll();
+  //     this._circle._rightAngle._horizontal.pulseScaleNow(1, 2);
+  //     this._circle._rightAngle._vertical.pulseScaleNow(1, 2);
+  //   } else {
+  //     this._circle._rightAngle.hideAll();
+  //     this._circle._angle.showAll();
+  //   }
+  //   this.diagram.animateNextFrame();
+  // }
 }
 
-export default ImportantAnglesCollection;
+export default SineCollection;
