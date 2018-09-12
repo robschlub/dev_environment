c3361e5cb8b4c674e644f7c5a107768a7c9b000a
rob <rschlub@gmail.com>
2018-09-11 17:20:09 -0700

    cleanup

--- src/Lessons/Math/Triangles/common/layout.js
+++ src/Lessons/Math/Triangles/common/layout.js
@@ -15,7 +15,7 @@ export default function commonLessonLayout() {
   const layout: Object = baseLayout();
   layout.colors = getCssColors(cssColorNames);
   layout.position = new Point(0, 0);
-  layout.lineWidth = 0.1;
+  layout.lineWidth = 0.2;
   layout.pointRadius = 0.2;
   layout.pointPositions = {
     p1: new Point(0, 1),
