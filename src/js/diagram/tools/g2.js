e06eee62283188a9348d9b9a1c4ba1ce67610a1a
Rob <rschlub@gmail.com>
2018-10-02 05:44:08 -0700

    remove Console from g2

--- src/js/diagram/tools/g2.js
+++ src/js/diagram/tools/g2.js
@@ -9,7 +9,7 @@
 import {
   roundNum, decelerate, clipMag, clipValue,
 } from './mathtools';
-import { Console } from '../../tools/tools';
+// import { Console } from '../../tools/tools';
 import * as m2 from './m2';
 
 // function nullDefaultNum(input: number | null, defaultValue: number): number {
