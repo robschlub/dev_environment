2ba3123176de51eab6929149d1819139c1d9c8be
rob <rschlub@gmail.com>
2018-10-02 07:16:42 -0700

    reshap quads

--- src/Lessons/Math/Quadrilaterals/common/layout.js
+++ src/Lessons/Math/Quadrilaterals/common/layout.js
@@ -19,30 +19,30 @@ export default function commonLessonLayout() {
   layout.quads = {
     quad1: {
       points: [
-        new Point(-0.5, -0.2),
-        new Point(-0.2, 0.5),
-        new Point(0.5, 0.2),
-        new Point(0.2, -0.4),
+        new Point(-0.5, -0.4),
+        new Point(-0.2, 0.7),
+        new Point(0.6, 0.1),
+        new Point(0.3, -0.6),
       ],
-      position: new Point(-1.5, 0),
+      position: new Point(-1.6, -0.2),
     },
     quad2: {
       points: [
-        new Point(-0.5, 0.2),
-        new Point(0.2, -0.4),
-        new Point(0.5, 0.5),
-        new Point(0, 0.1),
+        new Point(-0.6, 0.4),
+        new Point(0.2, -0.6),
+        new Point(0.6, 0.7),
+        new Point(0.1, 0.1),
       ],
-      position: new Point(0, 0),
+      position: new Point(0, -0.2),
     },
     quad3: {
       points: [
-        new Point(-0.5, -0.5),
-        new Point(0, -0.5),
-        new Point(0.5, 0.5),
-        new Point(0, 0.5),
+        new Point(-0.7, 0),
+        new Point(0, -0.7),
+        new Point(0.7, 0),
+        new Point(0, 0.7),
       ],
-      position: new Point(1.5, 0),
+      position: new Point(1.6, -0.2),
     },
   };
   return layout;
