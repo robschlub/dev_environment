e1490692cab2cef1d45f5d1aee3c06bdaf7e25f9
rob <rschlub@gmail.com>
2018-10-08 16:26:54 -0700

    modularized navigator icons

--- src/LessonsCommon/tools/equationNavigator.js
+++ src/LessonsCommon/tools/equationNavigator.js
@@ -12,6 +12,59 @@ import { Equation } from '../../js/diagram/DiagramElements/Equation/GLEquation';
 export type TypeEquationNavigator = {
 } & DiagramElementCollection;
 
+function makeArrow(
+  diagram: Diagram,
+  width: number,
+  height: number,
+  spacing: number,
+  color: Array<number>,
+  rotation: number,
+) {
+  const arrow = diagram.shapes.arrow(
+    width, 0, height, 0, color,
+    new Transform().translate(0, spacing * Math.cos(rotation)), new Point(0, 0), rotation,
+  );
+  return arrow;
+}
+
+function makeRefresh(
+  diagram: Diagram,
+  sides: number,
+  radius: number,
+  lineWidth: number,
+  angle: number,
+  color: Array<number>,
+) {
+  const refresh = diagram.shapes.collection(new Transform('Triangle')
+    .translate(0, 0));
+  const refreshTop = diagram.shapes.collection(new Transform('Triangle')
+    .rotate(0).translate(0, 0));
+
+  const refreshLine = diagram.shapes.polygon(
+    sides, radius,
+    lineWidth, (Math.PI - angle) / 2,
+    1, Math.floor(sides * angle / Math.PI / 2),
+    color, new Transform().translate(0, 0),
+  );
+  refreshTop.add('line', refreshLine);
+
+  const refreshArrow = diagram.shapes.arrow(
+    lineWidth * 3, 0, lineWidth * 3, 0,
+    color, new Transform().translate(
+      (radius + lineWidth) * Math.cos((Math.PI - angle) / 2),
+      (radius + lineWidth) * Math.sin((Math.PI - angle) / 2),
+    ), new Point(0, -lineWidth * 3), (Math.PI - angle) / 2 + Math.PI,
+  );
+  refreshTop.add('arrow', refreshArrow);
+
+  const refreshBottom = refreshTop._dup();
+  refreshBottom.transform.updateRotation(Math.PI);
+
+  refresh.add('top', refreshTop);
+  refresh.add('bottom', refreshBottom);
+  return refresh;
+}
+
 export default function makeEquationNavigator(
   diagram: Diagram,
   equation: eqn,
@@ -30,43 +83,40 @@ export default function makeEquationNavigator(
     .scale(1, 1)
     .translate(0, 0));
 
-  const prev = diagram.shapes.arrow(
-    arrowWidth, 0, arrowHeight, 0, color,
-    new Transform().translate(0, spacing), new Point(0, 0), 0,
+  const prev = makeArrow(diagram, arrowWidth, arrowHeight, spacing, color, 0);
+  const next = makeArrow(diagram, arrowWidth, arrowHeight, spacing, color, Math.PI);
+
+  const refresh = makeRefresh(
+    diagram, refreshSides, refreshRadius, refreshLineWidth,
+    refreshAngle, color,
   );
+  // const refresh = diagram.shapes.collection(new Transform('Triangle')
+  //   .translate(0, 0));
+  // const refreshTop = diagram.shapes.collection(new Transform('Triangle')
+  //   .rotate(0).translate(0, 0));
 
-  const next = diagram.shapes.arrow(
-    arrowWidth, 0, arrowHeight, 0, color,
-    new Transform().translate(0, -spacing), new Point(0, 0), Math.PI,
-  );
+  // const refreshLine = diagram.shapes.polygon(
+  //   refreshSides, refreshRadius,
+  //   refreshLineWidth, (Math.PI - refreshAngle) / 2,
+  //   1, Math.floor(refreshSides * refreshAngle / Math.PI / 2),
+  //   color, new Transform().translate(0, 0),
+  // );
+  // refreshTop.add('line', refreshLine);
 
-  const refresh = diagram.shapes.collection(new Transform('Triangle')
-    .translate(0, 0));
-  const refreshTop = diagram.shapes.collection(new Transform('Triangle')
-    .rotate(0).translate(0, 0));
+  // const refreshArrow = diagram.shapes.arrow(
+  //   refreshLineWidth * 3, 0, refreshLineWidth * 3, 0,
+  //   color, new Transform().translate(
+  //     (refreshRadius + refreshLineWidth) * Math.cos((Math.PI - refreshAngle) / 2),
+  //     (refreshRadius + refreshLineWidth) * Math.sin((Math.PI - refreshAngle) / 2),
+  //   ), new Point(0, -refreshLineWidth * 3), (Math.PI - refreshAngle) / 2 + Math.PI,
+  // );
+  // refreshTop.add('arrow', refreshArrow);
 
-  const refreshLine = diagram.shapes.polygon(
-    refreshSides, refreshRadius,
-    refreshLineWidth, (Math.PI - refreshAngle) / 2,
-    1, Math.floor(refreshSides * refreshAngle / Math.PI / 2),
-    color, new Transform().translate(0, 0),
-  );
-  refreshTop.add('line', refreshLine);
+  // const refreshBottom = refreshTop._dup();
+  // refreshBottom.transform.updateRotation(Math.PI);
 
-  const refreshArrow = diagram.shapes.arrow(
-    refreshLineWidth * 3, 0, refreshLineWidth * 3, 0,
-    color, new Transform().translate(
-      (refreshRadius + refreshLineWidth) * Math.cos((Math.PI - refreshAngle) / 2),
-      (refreshRadius + refreshLineWidth) * Math.sin((Math.PI - refreshAngle) / 2),
-    ), new Point(0, -refreshLineWidth * 3), (Math.PI - refreshAngle) / 2 + Math.PI,
-  );
-  refreshTop.add('arrow', refreshArrow);
-
-  const refreshBottom = refreshTop._dup();
-  refreshBottom.transform.updateRotation(Math.PI);
-
-  refresh.add('top', refreshTop);
-  refresh.add('bottom', refreshBottom);
+  // refresh.add('top', refreshTop);
+  // refresh.add('bottom', refreshBottom);
 
   navigator.add('prev', prev);
   navigator.add('next', next);
