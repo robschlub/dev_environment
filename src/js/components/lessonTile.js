0f391af79601bfbaab1069bb27db9d3bdf688485
rob <rschlub@gmail.com>
2018-10-11 08:27:06 -0700

    title is no longer shaddowed or clickable

--- src/js/components/lessonTile.js
+++ src/js/components/lessonTile.js
@@ -12,12 +12,17 @@ type Props = {
   link: ?string,
   imgLink: ?string,
   state: '' | 'disabled' | 'selected',
+  title: ?boolean,
 };
 
 export default class LessonTile extends React.Component
                                     <Props> {
   render() {
     const props = Object.assign({}, this.props);
+    let isTitle = false;
+    if (props.title != null) {
+      isTitle = props.title;
+    }
     // const Tag = props.href ? 'a' : 'button';
     const label = props.label || '';
     const id = props.id || '';
@@ -33,6 +38,9 @@ export default class LessonTile extends React.Component
 
     const link = props.link || '/';
     let classText = 'navigator__lesson_tile_containter navigator__lesson_shadow';
+    if (isTitle) {
+      classText = 'navigator__lesson_tile_containter navigator__lesson_tile_containter_title';
+    }
     if (props.state === 'disabled') {
       classText = `${classText} navigator__lesson_tile_disabled`;
     }
@@ -43,19 +51,29 @@ export default class LessonTile extends React.Component
     if (props.imgLink != null) {
       imgLink = `${'/static/dist'}${props.imgLink}`;
     }
-    return <a
-        href={link}
-        id={id}
-        style={style}
-        className="navigator__lesson_tile">
-      <div className={classText}>
+    const content = <div className={classText}>
         <img src={imgLink} className="navigator__lesson_tile_image" />
         <div className="navigator__lesson_tile_title_container">
           <div className="navigator__lesson_tile_title">
             {label}
           </div>
         </div>
-      </div>
+      </div>;
+
+    if (isTitle) {
+      return <div
+      id={id}
+        style={style}
+        className="navigator__lesson_tile">
+        {content}
+    </div>;
+    }
+    return <a
+        href={link}
+        id={id}
+        style={style}
+        className="navigator__lesson_tile">
+      {content}
     </a>;
   }
 }
