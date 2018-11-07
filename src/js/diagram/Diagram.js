5cc2444961bb0f1c80fec6bb9150bc7d36f7e03a
Rob <rschlub@gmail.com>
2018-11-07 11:24:11 -0800

    move diagram equations to separate file

--- src/js/diagram/Diagram.js
+++ src/js/diagram/Diagram.js
@@ -1,5 +1,4 @@
 // @flow
-// import * as vertexShapes from './DrawingObjects/VertexObject/vertexShapes';
 import WebGLInstance from './webgl/webgl';
 import getShaders from './webgl/shaders';
 
@@ -15,25 +14,8 @@ import GlobalAnimation from './webgl/GlobalAnimation';
 // eslint-disable-next-line import/no-cycle
 import Gesture from './Gesture';
 import DrawContext2D from './DrawContext2D';
-import {
-  DiagramFont,
-} from './DrawingObjects/TextObject/TextObject';
-
-import Integral from './DiagramElements/Equation/Integral';
-import Bracket from './DiagramElements/Equation/Bracket';
-import Bar from './DiagramElements/Equation/Bar';
-import SquareBracket from './DiagramElements/Equation/SquareBracket';
-import Brace from './DiagramElements/Equation/Brace';
-import RoundedSquareBracket from './DiagramElements/Equation/RoundedSquareBracket';
-
-import {
-  EquationForm, createEquationElements, Equation,
-} from './DiagramElements/Equation/GLEquation';
-// import type { EquationElementsType } from './DiagramElements/Equation/GLEquation';
-import HTMLEquation from './DiagramElements/Equation/HTMLEquation';
-
 import shapes from './DiagramPrimatives/shapes';
-// import { DiagramEquationNew } from './Equation';
+import equation from './DiagramEquations/equations';
 
 // There are several coordinate spaces that need to be considered for a
 // diagram.
@@ -50,184 +32,6 @@ import shapes from './DiagramPrimatives/shapes';
 // It is then transformed by the element colleciton
 // It is then transformed by the diagram
 // it is then transformed into GL Space
-
-// eslint-disable-next-line no-use-before-define
-function equation(diagram: Diagram, high: boolean = false) {
-  let webgl = diagram.webglLow;
-  let draw2D = diagram.draw2DLow;
-  if (high) {
-    webgl = diagram.webglHigh;
-    draw2D = diagram.draw2DHigh;
-  }
-  function elements(
-    elems: Object,
-    colorOrFont: Array<number> | DiagramFont = [],
-    firstTransform: Transform = new Transform('elements'),
-  ): DiagramElementCollection {
-    return createEquationElements(
-      elems,
-      draw2D,
-      colorOrFont,
-      diagram.limits,
-      firstTransform,
-    );
-  }
-
-  function vinculum(color: Array<number> = [1, 1, 1, 1]) {
-    return diagram.shapes.horizontalLine(
-      new Point(0, 0),
-      1, 1, 0,
-      color,
-      new Transform('vinculum').scale(1, 1).translate(0, 0),
-    );
-  }
-
-  function strike(color: Array<number> = [1, 1, 1, 1]) {
-    return diagram.shapes.horizontalLine(
-      new Point(0, 0),
-      1, 1, 0,
-      color,
-      new Transform('strike').scale(1, 1).rotate(0).translate(0, 0),
-    );
-  }
-
-  function xStrike(color: Array<number> = [1, 1, 1, 1]) {
-    const cross = diagram.shapes.collection(new Transform('strike').scale(1, 1).rotate(0).translate(0, 0));
-    cross.color = color;
-    const strike1 = diagram.shapes.horizontalLine(
-      new Point(0, 0),
-      1, 1, 0,
-      color,
-      new Transform('strike').scale(1, 1).rotate(0).translate(0, 0),
-    );
-    const strike2 = strike1._dup();
-    cross.add('s1', strike1);
-    cross.add('s2', strike2);
-    return cross;
-  }
-
-  function integral(
-    numLines: number = 1,
-    color: Array<number> = [1, 1, 1, 1],
-  ) {
-    return new Integral(
-      webgl,
-      color,
-      numLines,
-      new Transform('integral').scale(1, 1).translate(0, 0),
-      diagram.limits,
-    );
-  }
-
-  function bracket(
-    side: 'left' | 'right' | 'top' | 'bottom',
-    numLines: number = 1,
-    color: Array<number> = [1, 1, 1, 1],
-  ) {
-    return new Bracket(
-      webgl,
-      color,
-      side,
-      numLines,
-      new Transform('bracket').scale(1, 1).translate(0, 0),
-      diagram.limits,
-    );
-  }
-
-  function bar(
-    side: 'left' | 'right' | 'top' | 'bottom',
-    numLines: number = 1,
-    color: Array<number> = [1, 1, 1, 1],
-  ) {
-    return new Bar(
-      webgl, color, side, numLines,
-      new Transform('bar').scale(1, 1).translate(0, 0),
-      diagram.limits,
-    );
-  }
-
-  function squareBracket(
-    side: 'left' | 'right' | 'top' | 'bottom',
-    numLines: number = 1,
-    color: Array<number> = [1, 1, 1, 1],
-  ) {
-    return new SquareBracket(
-      webgl, color, side, numLines,
-      new Transform('bar').scale(1, 1).translate(0, 0),
-      diagram.limits,
-    );
-  }
-
-  function brace(
-    side: 'left' | 'right' | 'top' | 'bottom',
-    numLines: number = 1,
-    color: Array<number> = [1, 1, 1, 1],
-  ) {
-    return new Brace(
-      webgl, color, side, numLines,
-      new Transform('bar').scale(1, 1).translate(0, 0),
-      diagram.limits,
-    );
-  }
-
-  function roundedSquareBracket(
-    side: 'left' | 'right' | 'top' | 'bottom',
-    numLines: number = 1,
-    color: Array<number> = [1, 1, 1, 1],
-  ) {
-    return new RoundedSquareBracket(
-      webgl, color, side, numLines,
-      new Transform('bar').scale(1, 1).translate(0, 0),
-      diagram.limits,
-    );
-  }
-
-  function make(equationCollection: DiagramElementCollection) {
-    return new EquationForm(equationCollection);
-  }
-
-  function makeHTML(id: string = '', classes: string | Array<string> = []) {
-    return new HTMLEquation(id, classes);
-  }
-
-  function makeEqn() {
-    return new Equation(
-      draw2D,
-      diagram.limits,
-      // diagram.diagramToGLSpaceTransform,
-    );
-  }
-
-  function makeDescription(id: string) {
-    return diagram.shapes.htmlElement(
-      document.createElement('div'),
-      id,
-      'lesson__equation_description',
-      new Point(0, 0), 'middle', 'left',
-    );
-  }
-
-  return {
-    elements,
-    vinculum,
-    integral,
-    make,
-    makeHTML,
-    makeEqn,
-    strike,
-    xStrike,
-    makeDescription,
-    bracket,
-    bar,
-    squareBracket,
-    brace,
-    roundedSquareBracket,
-  };
-}
-
-// eslint-disable-next-line no-use-before-define
-
-
 class Diagram {
   canvasLow: HTMLCanvasElement;
   canvasHigh: HTMLCanvasElement;
@@ -397,7 +201,11 @@ class Diagram {
   }
 
   getEquations(high: boolean = false) {
-    return equation(this, high);
+    return equation(
+      this.webglLow, this.webglHigh, this.draw2DLow, this.draw2DHigh,
+      this.limits, this.shapes,
+      high,
+    );
   }
 
   sizeHtmlText() {
---
+++ src/js/diagram/DiagramEquations/equations.js
@@ -0,0 +1,205 @@
+// @flow
+import WebGLInstance from '../webgl/webgl';
+import {
+  Rect, Point, Transform,
+} from '../tools/g2';
+import {
+  DiagramElementCollection,
+} from '../Element';
+import DrawContext2D from '../DrawContext2D';
+import {
+  DiagramFont,
+} from '../DrawingObjects/TextObject/TextObject';
+
+import Integral from '../DiagramElements/Equation/Integral';
+import Bracket from '../DiagramElements/Equation/Bracket';
+import Bar from '../DiagramElements/Equation/Bar';
+import SquareBracket from '../DiagramElements/Equation/SquareBracket';
+import Brace from '../DiagramElements/Equation/Brace';
+import RoundedSquareBracket from '../DiagramElements/Equation/RoundedSquareBracket';
+
+import {
+  EquationForm, createEquationElements, Equation,
+} from '../DiagramElements/Equation/GLEquation';
+import HTMLEquation from '../DiagramElements/Equation/HTMLEquation';
+
+export default function equation(
+  webglLow: WebGLInstance,
+  webglHigh: WebGLInstance,
+  draw2DLow: DrawContext2D,
+  draw2DHigh: DrawContext2D,
+  limits: Rect,
+  shapes: HTMLElement,
+  high: boolean = false,
+) {
+  let webgl = webglLow;
+  let draw2D = draw2DLow;
+  if (high) {
+    webgl = webglHigh;
+    draw2D = draw2DHigh;
+  }
+  function elements(
+    elems: Object,
+    colorOrFont: Array<number> | DiagramFont = [],
+    firstTransform: Transform = new Transform('elements'),
+  ): DiagramElementCollection {
+    return createEquationElements(
+      elems,
+      draw2D,
+      colorOrFont,
+      limits,
+      firstTransform,
+    );
+  }
+
+  function vinculum(color: Array<number> = [1, 1, 1, 1]) {
+    return shapes.horizontalLine(
+      new Point(0, 0),
+      1, 1, 0,
+      color,
+      new Transform('vinculum').scale(1, 1).translate(0, 0),
+    );
+  }
+
+  function strike(color: Array<number> = [1, 1, 1, 1]) {
+    return shapes.horizontalLine(
+      new Point(0, 0),
+      1, 1, 0,
+      color,
+      new Transform('strike').scale(1, 1).rotate(0).translate(0, 0),
+    );
+  }
+
+  function xStrike(color: Array<number> = [1, 1, 1, 1]) {
+    const cross = shapes.collection(new Transform('strike').scale(1, 1).rotate(0).translate(0, 0));
+    cross.color = color;
+    const strike1 = shapes.horizontalLine(
+      new Point(0, 0),
+      1, 1, 0,
+      color,
+      new Transform('strike').scale(1, 1).rotate(0).translate(0, 0),
+    );
+    const strike2 = strike1._dup();
+    cross.add('s1', strike1);
+    cross.add('s2', strike2);
+    return cross;
+  }
+
+  function integral(
+    numLines: number = 1,
+    color: Array<number> = [1, 1, 1, 1],
+  ) {
+    return new Integral(
+      webgl,
+      color,
+      numLines,
+      new Transform('integral').scale(1, 1).translate(0, 0),
+      limits,
+    );
+  }
+
+  function bracket(
+    side: 'left' | 'right' | 'top' | 'bottom',
+    numLines: number = 1,
+    color: Array<number> = [1, 1, 1, 1],
+  ) {
+    return new Bracket(
+      webgl,
+      color,
+      side,
+      numLines,
+      new Transform('bracket').scale(1, 1).translate(0, 0),
+      limits,
+    );
+  }
+
+  function bar(
+    side: 'left' | 'right' | 'top' | 'bottom',
+    numLines: number = 1,
+    color: Array<number> = [1, 1, 1, 1],
+  ) {
+    return new Bar(
+      webgl, color, side, numLines,
+      new Transform('bar').scale(1, 1).translate(0, 0),
+      limits,
+    );
+  }
+
+  function squareBracket(
+    side: 'left' | 'right' | 'top' | 'bottom',
+    numLines: number = 1,
+    color: Array<number> = [1, 1, 1, 1],
+  ) {
+    return new SquareBracket(
+      webgl, color, side, numLines,
+      new Transform('bar').scale(1, 1).translate(0, 0),
+      limits,
+    );
+  }
+
+  function brace(
+    side: 'left' | 'right' | 'top' | 'bottom',
+    numLines: number = 1,
+    color: Array<number> = [1, 1, 1, 1],
+  ) {
+    return new Brace(
+      webgl, color, side, numLines,
+      new Transform('bar').scale(1, 1).translate(0, 0),
+      limits,
+    );
+  }
+
+  function roundedSquareBracket(
+    side: 'left' | 'right' | 'top' | 'bottom',
+    numLines: number = 1,
+    color: Array<number> = [1, 1, 1, 1],
+  ) {
+    return new RoundedSquareBracket(
+      webgl, color, side, numLines,
+      new Transform('bar').scale(1, 1).translate(0, 0),
+      limits,
+    );
+  }
+
+  function make(equationCollection: DiagramElementCollection) {
+    return new EquationForm(equationCollection);
+  }
+
+  function makeHTML(id: string = '', classes: string | Array<string> = []) {
+    return new HTMLEquation(id, classes);
+  }
+
+  function makeEqn() {
+    return new Equation(
+      draw2D,
+      limits,
+      // diagram.diagramToGLSpaceTransform,
+    );
+  }
+
+  function makeDescription(id: string) {
+    return shapes.htmlElement(
+      document.createElement('div'),
+      id,
+      'lesson__equation_description',
+      new Point(0, 0), 'middle', 'left',
+    );
+  }
+
+  return {
+    elements,
+    vinculum,
+    integral,
+    make,
+    makeHTML,
+    makeEqn,
+    strike,
+    xStrike,
+    makeDescription,
+    bracket,
+    bar,
+    squareBracket,
+    brace,
+    roundedSquareBracket,
+  };
+}
