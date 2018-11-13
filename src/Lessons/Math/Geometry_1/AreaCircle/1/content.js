382ea7a6702de946de3c992eb303a16607012dad
Rob <rschlub@gmail.com>
2018-11-13 09:01:18 -0800

    add height and base to content

--- src/Lessons/Math/Geometry_1/AreaCircle/1/content.js
+++ src/Lessons/Math/Geometry_1/AreaCircle/1/content.js
@@ -2,9 +2,9 @@
 import {
   LessonContent,
 } from '../../../../../js/Lesson/LessonContent';
-// import {
-//   click, centerV, highlight,
-// } from '../../../../../js/tools/htmlGenerator';
+import {
+  highlight, //click, centerV,
+} from '../../../../../js/tools/htmlGenerator';
 import LessonDiagram from './diagram';
 // import Definition from '../../../../LessonsCommon/tools/definition';
 import lessonLayout from './layout';
@@ -57,10 +57,10 @@ class Content extends LessonContent {
     common.showOnly = [circ];
     this.addSection(common, {
       title: 'Introduction',
-      setContent: ['The area of a circle can initially seem challenging to calculate as its edge is |curved|.'],
+      setContent: ['The area of a circle can initially seem challenging to calculate as its |edge is curved|.'],
       show: [circ._circle],
     });
-    common.setContent = 'Curves don\'t align with straight lines';
+    common.setContent = 'Curves don\'t line up with straight lines.';
     this.addSection(common, { show: [circ._circle] });
     this.addSection(common, { show: [circ._circle, circ._grid] });
     this.addSection(common, {
@@ -77,6 +77,23 @@ class Content extends LessonContent {
     this.addSection(common, { show: [circ._circle, circ._lines6] });
     this.addSection(common, { show: [circ._circle, circ._lines6, circ._poly6] });
     this.addSection(common, { show: [circ._backgroundCircle, circ._lines6, circ._poly6] });
+
+    common.setContent = 'Each triangle has a |height| and a |base|.';
+    common.modifiers = {
+      height: highlight(colors.height),
+      base: highlight(colors.border),
+    };
+    this.addSection(common, {
+      show: [
+        circ._backgroundCircle, circ._lines6, circ._poly6,
+      ],
+    });
+    this.addSection(common, {
+      show: [
+        circ._backgroundCircle, circ._lines6, circ._poly6,
+        circ._height6, circ._base6,
+      ],
+    });
 
     this.addSection(common, {
       title: 'Introduction',
